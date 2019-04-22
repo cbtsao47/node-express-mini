9d390545b0ed554ad85fd01563c0e5b9132489f7
wisdomtooth <cbtsao47@gmail.com>
2019-04-22 14:26:05 -0500

    initial commit, set up server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`\n🔥  Server listening on ${PORT} 🔥\n`);
+});
